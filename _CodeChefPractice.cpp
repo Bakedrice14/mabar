2b6df19f07890c36b270778dda9fa3cbee097b8c
Bakedrice14 <ahmadfauzirizky003@gmail.com>
2022-09-10 00:38:46 +0700

    solved _CodeChefPractice.cpp 'WA Test Cases'

--- _CodeChefPractice.cpp
+++ _CodeChefPractice.cpp
@@ -3,14 +3,32 @@
 using namespace std;
 int main()
 {
-    int t, x, y;
+    int min;
+    int t;
+    int n = INT_MAX;
+    string v;
     cin >> t;
     for (int i = 0; i < t; i++)
     {
-        int ans;
-        cin >> x >> y;
-        ans = abs(x - y);
-        cout << ans << endl;
+        min = INT_MAX;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        cin >> v;
+        for (int i = 0; i < v.length(); i++)
+        {
+            if (v[i] == '0')
+            {
+                if (a[i] < min)
+                {
+                    min = a[i];
+                }
+            }
+        }
+        cout << min << endl;
     }
     return 0;
 }
